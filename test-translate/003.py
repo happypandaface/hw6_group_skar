2f09912c33408746f16e65e09f763c6712f3a1c5
drkitty <bumblingkitty@gmail.com>
2016-03-15 16:00:02 -0700

    Add more tests

---
+++ test-translate/003.py
@@ -0,0 +1,4 @@
+a = True
+b = a or False
+c = not (False and b and b) or (a or b and True and not False)
+d = False and False and False
---
+++ test-translate/004.py
@@ -0,0 +1,8 @@
+a = 100
+b = True
+if False or b:
+    print a
+elif False:
+    print a, a
+else:
+    print a, a, a
