a68520c9c4e56e6f82040f81bea56491bb9c7553
Scott Griffy <ScottGriffy@gmail.com>
2016-03-18 18:58:57 -0700

    added a few more simple test cases

---
+++ test-translate/functions.py
@@ -0,0 +1,8 @@
+def func(a,b):
+  print a
+  print b
+  return a+b
+
+print func(5,6)
+if func(1,2)==3:
+  print 3
---
+++ test-translate/in.py
@@ -0,0 +1,7 @@
+print (5 in [4,5,6])
+a = [4,5,6]
+print (5 in a)
+print (3 in [4,5,6])
+print (3 in a)
+if 5 in a:
+  print 3
